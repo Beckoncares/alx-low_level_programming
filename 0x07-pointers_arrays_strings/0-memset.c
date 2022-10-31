82f50924d901f2b952629d73ff98808c499d1b96
Beckoncares <wolvesbeck@gmail.com>
2022-10-31 11:36:01 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * *_memset - fil memory with a constant byte
- * @s: memory area to be filed
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
  *
